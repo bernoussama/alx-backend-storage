0fdb1f0fd55409d1b176fbd65f548a8648075954
bernoussama <oussama.bernou@outlook.com>
2024-10-18 00:49:36 +0100

    update

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,7 +5,6 @@ CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
     DECLARE project_count INT DEFAULT 0;
     DECLARE project_id INT DEFAULT 0;
-
     SELECT COUNT(id)
         INTO project_count
         FROM projects
